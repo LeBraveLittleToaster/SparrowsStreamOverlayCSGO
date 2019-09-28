8f5b2f4ce427b55eae91988efc2a20e396095d32
Pascal Schiessle <pascal.schiessle@web.de>
2019-09-28 16:11:22 +0200

    Added basic logo rendering

---
+++ render_server/sparrowsrenderer/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WorldRenderer from './WorldRenderer';
+
+function App() {
+  return (
+    <WorldRenderer/>
+  );
+}
+
+export default App;
