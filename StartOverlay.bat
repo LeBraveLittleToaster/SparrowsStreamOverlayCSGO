ee3141ea604d17ddc435e5e511ab0c9ba2b807fe
PascalSchiessle <pascal.schiessle@web.de>
2019-10-12 16:42:50 +0200

    Added npm install script

--- StartOverlay.bat
+++ StartOverlay.bat
@@ -1,3 +1,3 @@
 start cmd.exe /k "cd cs_config && npm install && npm start"
-start cmd.exe /k "cd render_server\sparrowsrenderer && npm start"
+start cmd.exe /k "cd render_server\sparrowsrenderer && npm install && npm start"
 pause
