a450324dc03bc93f98924d84d319df26ce80c3ca
Jakob Meyer-Hilberg <jakob.meyer.hilberg@gmail.com>
2020-05-17 12:12:47 +0200

    basic router in App.js

--- client-sparrows-overlay/src/App.js
+++ client-sparrows-overlay/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div>
       <h1> Sparrows Overlay </h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/wait">Waiting</Link>
+              </li>
+              <li>
+                <Link to="/console"> Console </Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/wait">
+              <p> wait </p>
+            </Route>
+            <Route path="/console">
+              <p> console </p>
+            </Route>
+            <Route path="/">
+              <p> Home </p>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
