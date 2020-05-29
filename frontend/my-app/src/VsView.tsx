b947f13a7ed3bb20df95d5c197d43aa00bfa730c
Pascal Schiessle <pascal.schiessle@web.de>
2020-05-29 18:21:05 +0200

    frontend folder defines new ui

--- frontend/my-app/src/VsView.tsx
+++ frontend/my-app/src/VsView.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import './VsView.scss';
 
-function VsView() {
+interface VsViewProps {
+   score_a: number;
+     score_b: number;
+     logo_orga_path_a: string;
+     logo_orga_path_b: string;
+     logo_team_path_a: string;
+     logo_team_path_b: string;
+     team_name_a: string;
+     team_name_b: string;
+}
+
+function VsView(props:VsViewProps) {
   return (
     <div className="container">
+      <div className="vs-text">vs</div>
       <div className="announce-text">
         <a className="information-text">upcoming match</a>
       </div>
       <div className="team-top">
-        <div>
-          <div className="score">0</div>
-          <div className="orga-picture-container">
-            <img src="logo192.png" alt="smiley" className="orga-picture"/>
+        <div className="score">{props.score_a}</div>
+        <img src={props.logo_orga_path_a} alt="ups..." className="picture-orga" />
+        <img src={props.logo_team_path_a} alt="ups..." className="picture-team" />
+        <div className="team-name">
+          <div className="team-name-container">
+          <div className="team-name-text">{props.team_name_a}</div>
           </div>
-          <div className="team-picture"></div>
-          <div className="team-name"></div>
-        </div>
+          </div>
       </div>
       <div className="team-bottom">
-        <div>
-        <div className="score">0</div>
-        </div>
+      <div className="score">{props.score_a}</div>
+      <img src={props.logo_orga_path_b} alt="ups..." className="picture-orga" />
+        <img src={props.logo_team_path_b} alt="ups..." className="picture-team" />
+          <div className="team-name">
+          <div className="team-name-container">
+          <div className="team-name-text">{props .team_name_b}</div>
+          </div>
+          </div>
       </div>
       <div className="caster-text">
         <a>Caster: some and friends</a>
--- frontend/my-app/src/index.tsx
+++ frontend/my-app/src/index.tsx
@@ -6,7 +6,15 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <VsView />
+    <VsView 
+    score_a={0} 
+    score_b={1}
+    logo_orga_path_a="logo512.png"
+    logo_orga_path_b="logo512.png"
+    logo_team_path_a="logo512.png"
+    logo_team_path_b="logo512.png"
+    team_name_a="Ultra long teamname from team a"
+    team_name_b="more and more characters for teamname b"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
