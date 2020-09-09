805e7b0877d9ff7d00485104266daf59ac8f0e76
PascalSchiessle <pascal.schiessle@web.de>
2020-09-09 11:39:23 +0200

    Working on good structure, no clue yet

--- server/py/main.py
+++ server/py/main.py
@@ -1,9 +1,13 @@
-import time
+import time, jsonify
 from flask import Flask, request
 from updater import TextUpdater
 from queue import Queue
+from data import Data
 from updatewrapper import UpdateWrapper
 app = Flask(__name__)
+
+POSITION_TOP = "top"
+POSITION_BOTTOM = "bottom"
 
 q = Queue()
 upt = [
@@ -13,17 +17,36 @@ upt = [
 
 updater = UpdateWrapper(q, upt, args=())
 
-@app.route('/')
-def hello_world():
-    print("GET")
-    line = request.args.get('line', type=str)
-    index = request.args.get("i", type=int)
-    if line is not None and (index == 0 or index == 1) :
-        updater.queue.put((index, line))
-        return "Setting"
-    else:
-        return "Invalid parameter"
+data = Data()
+f = open("lines.json", "r")
+data.availableLines = f.readlines()
+f.close()
 
+@app.route('/names/set', methods=["GET"])
+def setNames():
+    rsp = { "enemy":False, "friendly":False }
+    enemy = request.args.get('enemy', type=str)
+    if enemy is not None and len(enemy) > 0:
+        data.enemy = enemy
+        rsp["enemy"] = True
+    friendly = request.args.get('friendly', type=str)
+    if friendly is not None and len(friendly) > 0:
+        data.friendly = friendly
+        rsp["friendly"] = True
+    return jsonify(rsp)
+
+@app.route('/names', methods=["GET"])
+def getNames():
+    return jsonify({"enemy":data.enemy, "friendly": data.friendly})
+
+@app.route('/lines', methods=["GET"])
+def getLines():
+    return jsonify({"lines": data.availableLines})
+
+@app.route('/lines/<string:position>/set', methods=["POST"])
+def setLinesActive(top):
+    json = request.get_json()
+    print(json)
 
 if __name__ == '__main__':
     updater.start()
